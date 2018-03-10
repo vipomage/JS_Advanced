d5fe063d3fc0dcce7d795e6f9fbd9a367fff86e3
vipomage <vipomage@gmail.com>
2018-03-10 12:35:53 +0200

    15.Lesson - PrototypeChain

---
+++ 15.PrototypeChain/01.PersonAndTeacher.js
@@ -0,0 +1,23 @@
+function personAndTeacher() {
+    class Person{
+        constructor(name,email){
+            this.name = name;
+            this.email = email;
+        }
+        toString(){
+            return `Person (name: ${this.name}, email: ${this.email})`
+        }
+    }
+    class Teacher extends Person{
+        constructor(name,email,subject){
+            super(name,email,subject);
+            this.subject = subject;
+        }
+        toString(){
+            return `Teacher (name: ${this.name}, email:${this.email}, subject:${this.subject})`
+        }
+    }
+
+    return {Person,Teacher}
+}
+
