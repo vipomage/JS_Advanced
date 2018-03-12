a21629ca5b7c704b46810eab6f0732a508cf6286
vipomage <vipomage@gmail.com>
2018-03-12 12:19:42 +0200

    05.ExtensibleClass

---
+++ 16.JsClassesAndPrototypeChain/05.ExtensibleClass.js
@@ -0,0 +1,34 @@
+
+let Extensible = (()=> {
+    let id = 0;
+    class Extensible {
+        constructor() {
+            this.id = id++;
+        }
+        extend(template){
+            for (let func in template) {
+                console.log(typeof(template[func]));
+                if (typeof(template[func]) === 'function') {
+                    this.constructor.prototype[func] = template[func]
+                }else{
+                    this[func] = template[func]
+                }
+            }
+        }
+
+    }
+
+    return Extensible;
+})();
+
+let template = {
+    extensionMethod: function () {
+
+    },
+    extensionProperty:'someString'
+}
+
+let obj1 = new Extensible();
+obj1.extend(template)
+console.log(obj1);
+
